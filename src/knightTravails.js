8fd0c18d5d06de05a63e185c8184f45def668085
elishamutang <elishamutang@outlook.com>
2024-06-21 19:56:30 +0800

    [update] Generated all possible moves at each location of chessboard.

--- src/knightTravails.js
+++ src/knightTravails.js
@@ -1,14 +1,21 @@
+import mergeSort from 'mergesort-by-eli'
+import tree from 'binary-search-tree-by-eli'
+
+// Set up node
+function node(data) {
+    return {
+        coordinate: data,
+        adjancencies: [],
+    }
+}
+
 // Set up chess gameboard.
 const chessboard = Array(8)
     .fill(0)
     .map((elem) => {
         elem = Array(8)
 
-        for (let i = 0; i < elem.length; i++) {
-            elem[i] = i
-        }
-
-        return elem
+        return elem.fill(0)
     })
 
 console.log('Chessboard')
@@ -18,24 +25,38 @@ function knightMoves() {
     // Build adjacency list. We can represent the relationship between each vertex and its adjacent nodes (neighbours) by using a BST.
     // The basic move of a knight is two steps forward and one step to the side or one step to the side and 2 steps forwards.
 
-    let adjacencyMatrix = Array(8)
-        .fill(0)
-        .map((elem) => {
-            elem = Array(8).fill(0)
+    let allMoves = []
 
-            return elem
+    const possibleMoves = (start) => {
+        let [a, b] = start
+        let newNode = new node(start)
+
+        newNode.adjancencies.push([a + 1, b + 2])
+        newNode.adjancencies.push([a + 2, b + 1])
+        newNode.adjancencies.push([a - 1, b - 2])
+        newNode.adjancencies.push([a - 2, b - 1])
+        newNode.adjancencies.push([a - 1, b + 2])
+        newNode.adjancencies.push([a - 2, b + 1])
+        newNode.adjancencies.push([a + 1, b - 2])
+        newNode.adjancencies.push([a + 2, b - 1])
+
+        newNode.adjancencies = newNode.adjancencies.filter((elem) => {
+            let [c, d] = elem
+
+            if (c >= 0 && d >= 0 && c < 7 && d < 7) return elem
         })
 
-    // Need to fill the matrix with 1s if it is a possible move.
-    adjacencyMatrix[1][2] = 1
-    adjacencyMatrix[2][1] = 1
-    adjacencyMatrix[2][4] = 1
-    adjacencyMatrix[0][4] = 1
-    adjacencyMatrix[4][0] = 1
-    adjacencyMatrix[4][2] = 1
+        allMoves.push(newNode)
+    }
 
-    console.log('Adj matrix')
-    console.log(adjacencyMatrix)
+    // Gather all possible moves from every location of the chessboard.
+    for (let i = 0; i < chessboard.length; i++) {
+        for (let j = 0; j < chessboard[i].length; j++) {
+            possibleMoves([i, j])
+        }
+    }
+
+    console.log(allMoves)
 }
 
 knightMoves()
