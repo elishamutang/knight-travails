e11d0c6c25bb702bbe22144424249d7cd6721541
elishamutang <elishamutang@outlook.com>
2024-06-22 01:07:57 +0800

    [update] Added a helper function to find position from allMoves array. Started working on BFS to search for path from start position to end position.

--- src/knightTravails.js
+++ src/knightTravails.js
@@ -1,6 +1,3 @@
-import mergeSort from 'mergesort-by-eli'
-import tree from 'binary-search-tree-by-eli'
-
 // Set up node
 function node(data) {
     return {
@@ -21,42 +18,81 @@ const chessboard = Array(8)
 console.log('Chessboard')
 console.log(chessboard)
 
-function knightMoves() {
-    // Build adjacency list. We can represent the relationship between each vertex and its adjacent nodes (neighbours) by using a BST.
-    // The basic move of a knight is two steps forward and one step to the side or one step to the side and 2 steps forwards.
+function knightMoves(start, end) {
+    // console.log(allMoves)
 
-    let allMoves = []
+    console.log(`Start: ${start}, End: ${end}`)
 
-    const possibleMoves = (start) => {
-        let [a, b] = start
-        let newNode = new node(start)
+    // Returns position object.
+    const findElem = (elem) => {
+        let [a, b] = elem
 
-        newNode.adjancencies.push([a + 1, b + 2])
-        newNode.adjancencies.push([a + 2, b + 1])
-        newNode.adjancencies.push([a - 1, b - 2])
-        newNode.adjancencies.push([a - 2, b - 1])
-        newNode.adjancencies.push([a - 1, b + 2])
-        newNode.adjancencies.push([a - 2, b + 1])
-        newNode.adjancencies.push([a + 1, b - 2])
-        newNode.adjancencies.push([a + 2, b - 1])
-
-        newNode.adjancencies = newNode.adjancencies.filter((elem) => {
-            let [c, d] = elem
-
-            if (c >= 0 && d >= 0 && c < 7 && d < 7) return elem
+        let [foundElem] = allMoves.filter((position) => {
+            if (position.coordinate[0] === a && position.coordinate[1] === b) return position
         })
 
-        allMoves.push(newNode)
+        return foundElem
     }
 
-    // Gather all possible moves from every location of the chessboard.
-    for (let i = 0; i < chessboard.length; i++) {
-        for (let j = 0; j < chessboard[i].length; j++) {
-            possibleMoves([i, j])
+    // Initiate the queue.
+    let queue = [findElem(start)]
+
+    let [row, col] = end
+
+    if (queue.length !== 0) {
+        let visitedPos = queue.shift()
+
+        // If end pos is in visitedPos adjancencies, checkForAdjacentNodes array length !== 0.
+        // Else, go through each adjacent nodes.
+        let checkForAdjacentNodes = visitedPos.adjancencies.filter((elem) => {
+            if (elem[0] === row && elem[1] === col) return elem
+        })
+
+        if (checkForAdjacentNodes.length === 0) {
+            if (visitedPos.coordinate[0] !== row && visitedPos.coordinate[1] !== col) {
+                queue.push(visitedPos.adjancencies[0])
+                knightMoves(visitedPos.adjancencies[0], end)
+            } else {
+                console.log(visitedPos)
+            }
         }
     }
 
-    console.log(allMoves)
+    // console.log('New queue')
+    // console.log(queue)
 }
 
-knightMoves()
+let allMoves = []
+
+const possibleMoves = (start) => {
+    let [a, b] = start
+    let newNode = new node(start)
+
+    newNode.adjancencies.push([a + 1, b + 2])
+    newNode.adjancencies.push([a + 2, b + 1])
+    newNode.adjancencies.push([a - 1, b - 2])
+    newNode.adjancencies.push([a - 2, b - 1])
+    newNode.adjancencies.push([a - 1, b + 2])
+    newNode.adjancencies.push([a - 2, b + 1])
+    newNode.adjancencies.push([a + 1, b - 2])
+    newNode.adjancencies.push([a + 2, b - 1])
+
+    newNode.adjancencies = newNode.adjancencies.filter((elem) => {
+        let [c, d] = elem
+
+        if (c >= 0 && d >= 0 && c < 7 && d < 7) return elem
+    })
+
+    allMoves.push(newNode)
+}
+
+// Gather all possible moves from every location of the chessboard.
+for (let i = 0; i < chessboard.length; i++) {
+    for (let j = 0; j < chessboard[i].length; j++) {
+        possibleMoves([i, j])
+    }
+}
+
+console.log(allMoves)
+
+knightMoves([0, 0], [3, 3])
