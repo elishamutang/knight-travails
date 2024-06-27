27c52d4f51b779b642b368767d663c0bae5d524f
elishamutang <elishamutang@outlook.com>
2024-06-27 21:51:16 +0800

    [update] Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import styles from './styles.css'
 import knightMoves from './knightTravails.js'
 
-let result = knightMoves([0, 0], [7, 7])
+let result = knightMoves([7, 0], [7, 7])
 
 console.log(`You made it in ${result.length - 1} moves! Here's your path:`)
 result.forEach((location) => {
