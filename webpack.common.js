e234b8d63ee94deb6bfb2829efcd6a358b10def7
elishamutang <elishamutang@outlook.com>
2024-06-27 20:55:03 +0800

    update

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/knightTravails.js',
+    entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
