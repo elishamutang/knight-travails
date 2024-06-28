e2f76defcd1a9a593fb021a857624591069d7301
elishamutang <elishamutang@outlook.com>
2024-06-28 08:31:02 +0800

    [update] Updated generateDOM.

--- src/interface.js
+++ src/interface.js
@@ -12,6 +12,7 @@ const generateChessboard = () => {
 
 // Generate DOM.
 export default function generateDOM() {
+    // Generate chessboard in the DOM.
     let chessboard = generateChessboard()
 
     const chessboardCon = document.getElementById('container')
@@ -25,7 +26,7 @@ export default function generateDOM() {
         for (let j = 0; j <= chessboard.length - 1; j++) {
             let box = document.createElement('div')
             box.className = 'box'
-            box.id = `box-${j}`
+            box.id = `${i},${j}`
 
             if (j % 2 === 0 && i % 2 === 0) {
                 box.style.background = 'black'
@@ -38,4 +39,11 @@ export default function generateDOM() {
             row.append(box)
         }
     }
+
+    // Add event listeners to the board.
+    chessboardCon.addEventListener('click', (e) => {
+        console.log(e.target)
+    })
+
+    // Allow users to choose a start and end point, then display the path from start to end.
 }
