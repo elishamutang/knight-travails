15291a9d1ef138e7d70cfa196a2ce6a3cf7f8353
elishamutang <elishamutang@outlook.com>
2024-06-29 17:03:55 +0800

    [update] Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import styles from './styles.css'
-import knightMoves from './knightTravails.js'
 import generateDOM from './interface.js'
-
-let result = knightMoves([0, 0], [2, 2])
-
-console.log(`You made it in ${result.length - 1} moves! Here's your path:`)
-result.forEach((location) => {
-    console.log(location)
-})
 
 generateDOM()
