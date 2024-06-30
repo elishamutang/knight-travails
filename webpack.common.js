64283e1a465f75cd09a1e7779cac6b0c20ef78a0
elishamutang <elishamutang@outlook.com>
2024-06-30 15:33:35 +0800

    [update] Updated package and webpack

--- webpack.common.js
+++ webpack.common.js
@@ -39,7 +39,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '',
+            title: 'Knight Travails',
             filename: 'index.html',
             template: 'src/template.html',
         }),
